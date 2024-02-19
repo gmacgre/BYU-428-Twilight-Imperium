bfe5f706b3403e5433f8782d44ea58813b7bb28e
miguelsanto0 <91389313+miguelsanto0@users.noreply.github.com>
2024-02-19 20:50:22 +0000

    Created a basic structure for the backend. Some visualization of what we can change/add later.

---
+++ server/src/com/twilightimperium/backend/Server.java
@@ -0,0 +1,17 @@
+package com.twilightimperium.backend;
+
+import java.net.http.HttpRequest;
+
+public class Server {
+    // Other attributes and methods
+
+    public void startServer() {
+        // Code to start server
+    }
+
+    public void handleRequest(HttpRequest request) {
+        // Code to handle HTTPS requests
+    }
+
+    // Other methods
+}
