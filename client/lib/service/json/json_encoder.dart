752b6e6c72a3e81d78e41ca64087fd891cb4b9f8
gmacgre <gmacgre@byu.edu>
2024-02-23 18:07:20 -0700

    Update JSONEncoder class

--- client/lib/service/json/json_encoder.dart
+++ client/lib/service/json/json_encoder.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:client/model/request_response/create/create_response.dart';
+import 'package:client/model/request_response/error_response.dart';
 import 'package:client/model/request_response/login/login_response.dart';
 
 class JSONEncoder {
@@ -13,4 +15,13 @@ class JSONEncoder {
   static LoginResponse decodeLoginResponse(String toDecode) {
     return LoginResponse.fromJson(jsonDecode(toDecode) as Map<String, dynamic>);
   }
+
+  static CreateResponse decodeCreateResponse(String toDecode) {
+    return CreateResponse.fromJson(jsonDecode(toDecode) as Map<String, dynamic>);
+  }
+
+  static ErrorResponse decodeErrorResponse(String toDecode) {
+    return ErrorResponse.fromJson(jsonDecode(toDecode) as Map<String, dynamic>);
+  }
+ 
 }
