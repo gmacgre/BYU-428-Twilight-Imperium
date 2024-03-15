613439dd38054ae297c70d5cb8944747b91c4c4c
Garrett Price <garrettprice27@gmail.com>
2024-03-15 15:13:49 -0600

    Make board look nicer

--- client/lib/board/planet.dart
+++ client/lib/board/planet.dart
@@ -1,4 +1,5 @@
 import 'package:client/data/planet_data.dart';
+import 'package:client/res/outlined_letters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -17,16 +18,15 @@ class _PlanetState extends State<Planet> {
   late OverlayEntry? entry;
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     return Center(
       key: key,
       child: ConstrainedBox(
-        constraints: BoxConstraints.tight(
-          const Size(
-            40,
-            40,
-          ),
-        ),
+        constraints: BoxConstraints.tight(widget.planet.name != "Mecatol Rex"
+            ? const Size(
+                40,
+                40,
+              )
+            : const Size(80, 80)),
         child: MouseRegion(
           onEnter: _onEnter,
           onExit: _onExit,
@@ -34,8 +34,8 @@ class _PlanetState extends State<Planet> {
             height: double.infinity,
             width: double.infinity,
             decoration: BoxDecoration(
-                borderRadius: BorderRadiusDirectional.circular(width * .02),
-                color: Colors.red,
+                borderRadius: BorderRadiusDirectional.circular(40),
+                color: widget.planet.color,
                 border: Border.all(
                   //This will be set to the controlling player's color
                   color: Colors.black,
@@ -62,10 +62,10 @@ class _PlanetState extends State<Planet> {
     Offset position = box.localToGlobal(Offset.zero);
     double x = position.dx;
     double y = position.dy;
-    double yAdjustment = 170;
-    double xAdjustment = 200;
-    double top = y - yAdjustment;
-    double left = x - xAdjustment;
+    double yAdjustment = 0;
+    double xAdjustment = 70;
+    double top = y + yAdjustment;
+    double left = x + xAdjustment;
     if (top <= 10) {
       top = 10;
     }
@@ -75,14 +75,33 @@ class _PlanetState extends State<Planet> {
           top: top,
           left: left,
           child: Container(
-            width: 200,
-            color: Colors.cyan,
-            child: Column(
-              children: [
-                Text(widget.planet.name),
-                Text('${widget.planet.resources}'),
-                Text('${widget.planet.influence}'),
-              ],
+            width: 150,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(
+                200,
+                100,
+                100,
+                100,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: DefaultTextStyle(
+              style: const TextStyle(
+                fontFamily: 'Handel Gothic D',
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  OutlinedLetters(
+                    content: widget.planet.name,
+                    fontSize: 18,
+                  ),
+                  Text('Resources: ${widget.planet.resources}'),
+                  Text('Influence: ${widget.planet.influence}'),
+                ],
+              ),
             ),
           ),
         );
