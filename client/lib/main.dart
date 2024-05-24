22e7287847a1670a998e73591559df061c4ed119
gmacgre <gmacgre@byu.edu>
2024-05-23 19:08:45 -0600

    Refactor game_page.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:client/board/board_grid.dart';
-import 'package:client/board/game_page.dart';
+import 'package:client/pages/game_page.dart';
 import 'package:client/combat/combat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:client/pages/create_join/create_join.dart';
