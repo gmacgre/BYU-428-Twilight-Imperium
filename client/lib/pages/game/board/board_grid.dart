9ce3367bfe5847c239364c5620545ca609cf5894
gmacgre <gmacgre@byu.edu>
2024-05-27 22:50:13 -0600

    Redo Command Widgets

--- client/lib/pages/game/board/board_grid.dart
+++ client/lib/pages/game/board/board_grid.dart
@@ -38,44 +38,46 @@ class _BoardGridState extends ConsumerState<BoardGrid> {
       return const CombatPage(state: CombatState.enteringCombat);
     }
 
-    return Container(
+    return SizedBox(
       height: double.infinity,
       width: double.infinity,
-      padding: const EdgeInsets.all(10),
       child: InteractiveViewer(
-        child: HexagonGrid.flat(
-          depth: widget._depth,
-          padding: EdgeInsets.zero,
-          buildTile: (coordinates) => HexagonWidgetBuilder(
-            padding: 2.0,
-            cornerRadius: 0,
-            color: const Color.fromARGB(255, 0, 0, 100),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: HexagonGrid.flat(
+            depth: widget._depth,
+            padding: EdgeInsets.zero,
+            buildTile: (coordinates) => HexagonWidgetBuilder(
+              padding: 2.0,
+              cornerRadius: 0,
+              color: const Color.fromARGB(255, 0, 0, 100),
+            ),
+            buildChild: (coordinates) {
+              //Adding the _depth is necessary to force everything to be a positive value.
+              //The hex grid makes the origin (0,0) the middle of the grid, but since
+              //lists can't have negative indexes we need to adjust accordingly.
+              //To translate from model to view, subtract _depth to coordinates
+              //To transalte from view to model, add _depth from coordinates
+              return BoardSpace(
+                coordinate: Coords(
+                  coordinates.q + widget._depth,
+                  coordinates.r + widget._depth,
+                ),
+                systemState: systems[(coordinates.q + widget._depth)]
+                    [(coordinates.r + widget._depth)],
+                activated: (activeCoordinate?.x ==
+                        coordinates.q + widget._depth &&
+                    activeCoordinate?.y ==
+                        coordinates.r + widget._depth),
+                selected: (selectedCoordinate?.x ==
+                        coordinates.q + widget._depth &&
+                    selectedCoordinate?.y ==
+                        coordinates.r + widget._depth),
+                highlight: highlightable.contains(Coords(coordinates.q + widget._depth,
+                  coordinates.r + widget._depth))
+              );
+            },
           ),
-          buildChild: (coordinates) {
-            //Adding the _depth is necessary to force everything to be a positive value.
-            //The hex grid makes the origin (0,0) the middle of the grid, but since
-            //lists can't have negative indexes we need to adjust accordingly.
-            //To translate from model to view, subtract _depth to coordinates
-            //To transalte from view to model, add _depth from coordinates
-            return BoardSpace(
-              coordinate: Coords(
-                coordinates.q + widget._depth,
-                coordinates.r + widget._depth,
-              ),
-              systemState: systems[(coordinates.q + widget._depth)]
-                  [(coordinates.r + widget._depth)],
-              activated: (activeCoordinate?.x ==
-                      coordinates.q + widget._depth &&
-                  activeCoordinate?.y ==
-                      coordinates.r + widget._depth),
-              selected: (selectedCoordinate?.x ==
-                      coordinates.q + widget._depth &&
-                  selectedCoordinate?.y ==
-                      coordinates.r + widget._depth),
-              highlight: highlightable.contains(Coords(coordinates.q + widget._depth,
-                coordinates.r + widget._depth))
-            );
-          },
         ),
       ),
     );
