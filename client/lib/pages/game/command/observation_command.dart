93a8131943ff1f67d31b5d3b09d5fef2604f7154
gmacgre <gmacgre@byu.edu>
2024-05-30 22:05:57 -0600

    Add Observation Command Widget

--- client/lib/pages/game/command/observation_command.dart
+++ client/lib/pages/game/command/observation_command.dart
@@ -1,11 +1,14 @@
+import 'package:client/model/riverpod/board_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ObservationCommandWidget extends StatelessWidget {
+class ObservationCommandWidget extends ConsumerWidget {
   const ObservationCommandWidget({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    int player = ref.read(boardStateProvider).activePlayer;
+    return Center(
+      child: Text('Waiting for Player ${player + 1}...'),
+    );
   }
 }
